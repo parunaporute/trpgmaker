fdb5f7be2d21870d9a78f86068154c7c430b3c6a
parunaporute <parunaporute@gmail.com>
2025-02-12 03:03:59 +0900

    htmlのジェネレータを採用

---
+++ htmlGen/render.js
@@ -0,0 +1,34 @@
+const nunjucks = require('nunjucks');
+const fs = require('fs');
+const path = require('path');
+
+// 出力先フォルダのパス
+const distPath = path.join(__dirname, '../');
+
+// dist フォルダが存在しない場合は作成
+if (!fs.existsSync(distPath)) {
+    fs.mkdirSync(distPath, { recursive: true });
+}
+
+// Nunjucks の設定
+nunjucks.configure('views', { autoescape: true });
+
+// データを渡してレンダリング
+const outputIndex           = nunjucks.render('index.njk');
+const outputBookshelf       = nunjucks.render('bookshelf.njk');
+const outputCharacterCreate = nunjucks.render('characterCreate.njk');
+const outputPartyCreate     = nunjucks.render('partyCreate.njk');
+const outputPartyList       = nunjucks.render('partyList.njk');
+const outputScenario        = nunjucks.render('scenario.njk');
+const outputScenarioWizard  = nunjucks.render('scenarioWizard.njk');
+
+// 結果をファイルに保存
+fs.writeFileSync(path.join(distPath, 'index.html'), outputIndex);
+fs.writeFileSync(path.join(distPath, 'bookshelf.html'), outputBookshelf);
+fs.writeFileSync(path.join(distPath, 'characterCreate.html'), outputCharacterCreate);
+fs.writeFileSync(path.join(distPath, 'partyCreate.html'), outputPartyCreate);
+fs.writeFileSync(path.join(distPath, 'partyList.html'), outputPartyList);
+fs.writeFileSync(path.join(distPath, 'scenario.html'), outputScenario);
+fs.writeFileSync(path.join(distPath, 'scenarioWizard.html'), outputScenarioWizard );
+
+console.log('HTML を ' + distPath + 'に出力しました');
